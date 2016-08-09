5198a58bc0cbbe7985cd71b990f7e1cf7e2705b4
Rich Downie <rich@blackboxd.com>
2016-08-09 11:11:34 -0400

    adding applitools eyes_selenium bits

--- features/step_definitions/authentication_steps.rb
+++ features/step_definitions/authentication_steps.rb
@@ -6,4 +6,10 @@ Given /I login with an Invalid User/i  do
   @login_page.login=("richdownie")
   @login_page.password=("new_yorker")
   @login_page.login.click
+  #Enable Applitools below to verify the Login Page is styled correctly
+  # eyes = Applitools::Eyes.new
+  # eyes.api_key = 'api_key'git 
+  # eyes.test(app_name: 'E-xact Transactions Ltd. - Please Log In', test_name: 'E-xact Transaction Login page', viewport_size: {width: 1024, height: 768}, driver: @browser) do |driver|
+  #   eyes.check_window('E-xact Transactions Ltd. - Please Log In')
+  # end
 end
